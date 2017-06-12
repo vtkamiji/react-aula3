c0b1cb3e0a5cc23f994bbd515878c359e91fe5e3
vtkamiji <vtkamiji@gmail.com>
2017-06-12 14:07:52 -0300

    push final

--- src/reducers/reducer-weather.js
+++ src/reducers/reducer-weather.js
@@ -1,12 +1,9 @@
 import {FETCH_WEATHER} from '../actions/index';
 
 export default function(state = [], action){
-	console.log("action received:" + action);
-	console.log(action.payload);
-	console.log(action.type);
+	
 	switch(action.type) {
-		case FETCH_WEATHER:
-			console.log('entered switchcase');
+		case FETCH_WEATHER:			
 			/* Do NOT ever manipulate state direct */
 			//return state.push(action.payload.data); **cria novo state
 			return state.concat([action.payload.data]);
